5082beb996af6ec1860360612f382322547adb34
ambition_echo <ambition_echo@outlook.com>
2023-01-04 19:47:01 +0800

    fix

--- api/get_commit_list.js
+++ api/get_commit_list.js
@@ -1,7 +1,7 @@
 import db from "./db";
 
 export default function handler(req, res) {
-    const { creator, assigned } = req;
+    const { creator, assigned } = req.query;
     let response = [];
     if (typeof (creator) != "undefined") {
         response = db.Commit.filter(x => {
