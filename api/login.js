0eaaf0df4c163af02267af6f91934f503f6d9fbd
ambition_echo <ambition_echo@outlook.com>
2023-01-05 15:37:56 +0800

    update

---
+++ api/login.js
@@ -0,0 +1,22 @@
+import db from "./utils/db";
+import cors from "./utils/cors";
+
+const handler = (req, res) => {
+    const { user_name, password } = req.query;
+
+    if (typeof (user_name) != "undefined") {
+        if (typeof (password) != "undefined") {
+            const user = db.User.filter(x => {
+                return x.user_name == user_name;
+            })
+            if (length(user) != 0) {
+                if (user[0].password == password) {
+                    return res.json(user[0]);
+                }
+            }
+        }
+    }
+    return res.json({});
+}
+
+module.exports = cors(handler)
